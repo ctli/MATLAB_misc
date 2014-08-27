012808eac83ba912f730a929ffc38b0379d30bd2
ctli <ctli@umich.edu>
2014-08-27 10:46:25 -0400

    generation adaquacy

--- china_ele_regions.m
+++ china_ele_regions.m
@@ -2,6 +2,8 @@ clear all
 close all
 clc
 
+region = {'Northeast', 'Inner Mongolia', 'Northwest', 'North', 'East', 'Central', 'South'};
+
 capacity = [
     107 % northeast, [GW]
      56 % inner mongolia
@@ -20,7 +22,24 @@ peak_demand = [
     122
     113];
 
-generation_adaquacy.value = peak_demand./capacity;
-generation_adaquacy.region = {'northeast', 'inner mongolia', 'northwest', 'north', 'east', 'central', 'south'};
+generation_adaquacy = peak_demand./capacity;
 
+% ==============================
 figure(1); clf;
+bar(generation_adaquacy, 'facec', [0.4 0.4 0.4], 'edge', 'none');
+ylim([0 1]);
+xlim([0.4 7.6]);
+goldenratio;
+set(gca, 'fontsize', 8);
+ylabel('Generation Adaquacy (-)');
+h = my_xticklabels(gca, 1:7, region, 'Rotation',25, 'horizontalalignment', 'right', 'fontsize', 8);
+box off;
+my_gridline('y');
+set(gca, 'layer', 'bottom');
+set(gca, 'pos', [0.1300    0.2017    0.8335    0.7401]);
+text(2,0.03, 'N/A', 'rotation', 90, 'fontsize', 9, 'fontweight', 'bold');
+text(0.65, 0.92, 'Peak/Capacity Ratio')
+% export_fig generation_adaquacy -painters;
+
+
+
